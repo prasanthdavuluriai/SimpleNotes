307dd85a09c6c3a66beb4d3d54decc39763b3465
PrasanthDavuluriAI <prasanthdavuluriai@gmail.com>
2026-01-20 19:35:55 -0500

    Build failure

--- app/src/main/java/com/simplenotes/NoteActivity.java
+++ app/src/main/java/com/simplenotes/NoteActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import java.util.List;
 import android.text.style.StyleSpan;
 import android.graphics.Typeface;
